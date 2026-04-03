ba9de73756c9fd5c9c3da33449376376bf11729a
alaa alkhatib <mswamr12345com@gmail.com>
2026-04-04 00:40:54 +0300

    donne

--- ex2/ft_different_errors.py
+++ ex2/ft_different_errors.py
@@ -5,11 +5,11 @@ def garden_operations(operation_number: int) -> None:
     if operation_number == 0:
         int("abc")
     elif operation_number == 1:
-        result = 10 / 0
+        _ = 10 / 0
     elif operation_number == 2:
         open("/non/existent/file")
     elif operation_number == 3:
-        result = "garden" + 42  # type: ignore
+        _ = "garden" + 42  # type: ignore
 
 
 def test_error_types() -> None:
